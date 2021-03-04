6f14c7b56370e158feb54d8a138521c2bb2609fe
jkelly101 <jenLkelly@gmail.com>
2021-03-04 17:16:11 -0500

    added ID to db.json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 var express = require("express");
 var path = require("path");
+const { nextTick } = require("process");
 
 // Sets up the Express App
 var app = express();
@@ -11,6 +12,10 @@ app.use(express.json());
 
 // Routes
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+  });
+  
 // Starts server
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
